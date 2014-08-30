07c2b2b464fdf179d425b790e872bda861fb20a4
Konstantin <Sipatuj@gmail.com>
2014-08-30 09:29:39 +0300

    minor changes

---
+++ inc/contacts.php
@@ -0,0 +1,19 @@
+<div class="contacts">
+    <h3>Напишите мне</h3>
+    <form method="POST" id="feedback-form">
+        <p>Как к Вам обращаться:</p>
+        <br>
+        <input type="text" name="nameFF" required placeholder="фамилия имя отчество">
+        <p>Email для связи:
+            <p>
+                <input type="email" name="contactFF" required placeholder="адрес электронной почты">
+                <p>Анти бот:
+                    <p>
+                        <input type="antiBot" name="antiBotFF" required placeholder="введите там что-то">
+                        <p>Ваше сообщение:
+                            <p>
+                                <textarea name="messageFF" required rows="5"></textarea>
+                                <input type="submit" value="отправить">
+    </form>
+</div>
+<!--end contacts-->
